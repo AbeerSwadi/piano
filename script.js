beaf96415b4f6ccf749349588c2b0fda0158946e
AbeerSwadi <141174677+AbeerSwadi@users.noreply.github.com>
2023-09-04 00:26:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function play(num) {
+    var aud = new Audio(src = "note" + num + ".wav")
+    aud.play()
+    console.log("mm")
+    if (num == 1) {
+        console.log("♩")
+        alert("♩")
+    }
+    else if (num == 2) {
+
+    }
+
+
+}
+console.log("♩")
